accf660e3ccd966bad18eef92fdb18252f052c36
anand gupta <90339262+DevilANANDGupta@users.noreply.github.com>
2023-01-30 14:27:44 +0530

    Update email.py
    New code with attachment of file

--- email_part/email.py
+++ email_part/email.py
@@ -1,2 +1,26 @@
-print("Hello")
-print("Hello")
+import randomimage as rd
+l=rd.k
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from pathlib import Path
+import smtplib
+from playsound import playsound
+import time
+
+message = MIMEMultipart()
+message["from"]="0201csai107@niet.co.in"
+message["to"]="ag075261@gmail.com"
+message["subject"] = "Hey bro"
+message.attach(MIMEText("Body"))
+message.attach(MIMEImage(Path(l).read_bytes()))
+with smtplib.SMTP(host="smtp.outlook.com",port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login("0201csai107@niet.co.in", "niet@1234")
+    smtp.send_message(message)
+
+    print("sent...................................100%")
+    playsound('D:\email_sender\danger-alarm-23793.mp3')
+    # else:
+    #     print("message not sent")
